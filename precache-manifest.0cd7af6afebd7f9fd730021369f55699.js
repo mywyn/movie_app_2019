8b3169889e0060b52354eb6d796a30c21edf806f
mfz <mofatz@gmail.com>
2019-11-23 18:03:53 +0900

    Updates

---
+++ precache-manifest.0cd7af6afebd7f9fd730021369f55699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edb6e0ad66c7a206ed7b2db079c71e0",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "d902c9efc0aa22691ed0",
+    "url": "/movie_app_2019/static/css/main.59125477.chunk.css"
+  },
+  {
+    "revision": "18e17f1c73cef50e947f",
+    "url": "/movie_app_2019/static/js/2.e25caf30.chunk.js"
+  },
+  {
+    "revision": "d902c9efc0aa22691ed0",
+    "url": "/movie_app_2019/static/js/main.60258b7b.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
